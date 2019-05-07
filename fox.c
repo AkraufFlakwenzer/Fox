4ee235904f2d64dbdebda485101c1dfbb0826ed8
akrauf <43223544+AkraufFlakwenzer@users.noreply.github.com>
2019-05-07 11:58:07 -0500

    Update and rename Fox.c to fox.c

---
+++ fox.c
@@ -0,0 +1,13 @@
+/*
+ *  fox.c, v0.1.0; 2019/05/09 11:54 akrauf
+ *  Fox stand-alone interpreter
+ *  See Copyright notice in fox.h
+ */
+
+#define fox_c
+
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
